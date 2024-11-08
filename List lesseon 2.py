d2e33811caeb9f02e331a433e9abbc27a903a777
CyberWarrior404 <mdmor@icloud.com>
2024-11-08 16:59:50 +0400

    Add files via upload

---
+++ List lesseon 2.py
@@ -0,0 +1,17 @@
+List = [0,1,2,3,4,5,6,7,8,9,10]
+print(List)
+
+print(List[0:11:1])
+print(List[11:0:-1])
+#Check for Palindrome
+
+Word = input("lemme check ur word if its a palindrome ")
+
+Word[::-1]
+
+if Word == Word [::-1]:
+    print("its a Palindrome")
+else:
+    print("DONT WASTE MY TIME U WILL NEVER SEE ME UNTIL U SOMEHOW RESTART ME")
+
+
